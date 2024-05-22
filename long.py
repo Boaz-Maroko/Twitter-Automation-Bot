c142ff19d240b4b52dbd3f5ca4cde8bc2e96f856
Boaz Maroko <125216748+Boaz-Maroko@users.noreply.github.com>
2024-05-22 20:01:38 +0300

    Delete long.py

--- long.py
+++
@@ -1,7 +0,0 @@
-# consumer_key 
-api_key = "ak0pUbzNqoWBjZfeypyqQbnug"
-api_key_secret = "zvQ0YJSlr2Cv87zoJqe5xEJnx345SGWTGnLdQPk84u7pgLBNEM"
-access_token = "1340957699605983234-o3XOXaCvjxfBZ1BqnAqWXF0DHQN8uS"
-access_token_secret = "1RRYgeoAMPrewiQClpELpvZOkr026aWslfcOqluynlIvY"
-
-bearer_token = "AAAAAAAAAAAAAAAAAAAAANyOtwEAAAAAQkRPFBIrjr8Zz%2B91j17s1TqdLgY%3D1jIKmLQ9yhnsYAMB8lq1I8dL3j7cFKSDmm5zulqIVot8Zmj89K"
